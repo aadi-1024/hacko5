f9ae0de1217c0ff9c6953f2a8837fd42ef80a442
Gopal <82232811+gopal-ag@users.noreply.github.com>
2023-04-15 22:00:16 +0530

    Add files via upload

--- Thermal_face.py
+++ Thermal_face.py
@@ -13,10 +13,13 @@ while True:
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    img = 255 - img
+    cv2.applyColorMap(img, cv2.COLORMAP_MAGMA, img)
     if d != '0':
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            cv2.putText(img, f'Face detected', (x, y-8), color=(0, 0, 255), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(img, f'Intrusion Detected', (x, y-8), color=(0,
+                        255, 0), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1)
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
     if k==27:
